86c7038866e1295746ceed4dd186b6cefac557b7
Harshit Katyal <harshit1121998@gmail.com>
2019-04-16 00:51:11 +0530

    db and backend complete

--- app.js
+++ app.js
@@ -19,82 +19,23 @@ mongoose.connect("mongodb://localhost/mentor-mentee")// mentor-mentee is the nam
 app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(passport.initialize())
-app.use(passport.session())
+
+// order of middleware is imp bcos passport.session() depends on express-session()
 app.use(require('express-session')({
     secret: 'I am good boy',
     resave: false,
     saveUninitialized: false
 }))
 
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 passport.use(new LocalStrategy(User.authenticate()))
 //resp for decoding and encoding the session
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
-
-/*----------------------Schema-------------------*/
-var mmSchema = new mongoose.Schema({
-    name: String,
-    subject: String
-})
-
-/* ---------------------Compiling Models------------ */
-var mentor = mongoose.model("Mentor", mmSchema)
-
-var mentee = mongoose.model("Mentor", mmSchema)
-
-// adding a new dog
-// var george = new Dog({
-//     name: 'George',
-//     age: 11,
-//     temperament: 'Grouchy'
-// })
-
-// george.save((err, dog) => {
-//     if (err) {
-//         console.log('something went wrong')
-//     }
-
-//     else {
-//         console.log('dog saved')
-//         console.log(dog)
-//         // dog is what has been saved to the database
-//     }
-// })
-
-// another way using create()
-
-// Dog.create({
-//     name: 'Snowy',
-//     age: 3,
-//     temperament: 'Cool'
-// }, (err, dog) => {
-//     if (err) {
-//         console.log('OH NO, ERROR!')
-//         console.log(err)
-//     }
-
-//     else {
-//         console.log('all the cats : ')
-//         console.log(dog)
-//     }
-// })
-
-// retrieve data from db
-// Dog.find({}, (err, dog) => {
-//     if (err) {
-//         console.log('OH NO, ERROR!')
-//         console.log(err)
-//     }
-
-//     else {
-//         console.log('all the cats : ')
-//         console.log(dog)
-//     }
-// })
-
-
+// custom middleware
 
 
 
@@ -104,11 +45,15 @@ var mentee = mongoose.model("Mentor", mmSchema)
 
     Each route can have 1 or more handler functions which are executed when the route is matched
 */
-app.get('/', (req, res) => res.render('index'))
-app.get('/index', (req, res) => res.render('index'))
+app.get('/', (req, res) => res.render('home'))
 app.get('/register', (req, res) => res.render('register'))
 app.get('/login', (req, res) => res.render('login'))
-app.get('/about', (req, res) => res.render('about'))
+app.get('/profile', isLoggedIn, (req, res) => res.render('profile'))
+app.get('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/')
+})
+
 
 app.post('/register', (req, res) => {
 
@@ -120,14 +65,26 @@ app.post('/register', (req, res) => {
         }
 
         passport.authenticate('local')(req, res, () => {
-            res.redirect('/index')
+            res.redirect('/login')
         })
     })
 })
 
 app.post('/login', passport.authenticate('local', {
-    successRedirect: '/secret',
-    failureRedirect: '/index'
+    successRedirect: '/profile',
+    failureRedirect: '/secret'
 }))
 
-app.listen(3000, () => console.log('Server has started'))
+
+// custom middleware
+function isLoggedIn(req, res, next) {
+
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    res.redirect("/");
+}
+
+
+app.listen(3000, () => console.log('Server has started...'))
